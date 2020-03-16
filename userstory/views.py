0f38f820c6f4897faecc949e841e5649d081e0cc
Abhishek Ahuja <abhishek1ahuja@gmail.com>
2020-03-16 23:54:39 +0530

    remove duplicate code in views.py add transition from APPROVED/REJECTED to DRAFT state

--- userstory/views.py
+++ userstory/views.py
@@ -9,41 +9,63 @@ from .serializers import *
 
 import datetime
 
+def check_user_validity(request):
+    username = request.query_params.get('user', None)
+    if username == None:
+        return 1
+    try:
+        user = User.objects.get(username=username)
+        return user
+    except User.DoesNotExist:
+        return 2
+
+error_messages = {
+                'user':{'1': 'specify user=<username> as a query parameter',
+                        '2': 'User with this username does not exist'
+                        },
+                'story':{'2': 'Story with this id does not exist'
+                         }
+                }
+
+def check_story_validity(story_id):
+    try:
+        story = Story.objects.get(pk=story_id)
+        return story
+    except Story.DoesNotExist:
+        return 2
+
 @api_view(['GET', 'POST'])
 def stories_list(request):
     """
     List all products, or create a new product.
     """
-    username = request.query_params.get('user', None)
+    user = check_user_validity(request)
+    if(type(user)) != User:
+        return Response( {'error': error_messages['user'][user] }, status=status.HTTP_400_BAD_REQUEST )
     story_status = request.query_params.get('status', None)
-    if username == None:
-        return Response({'error':'specify user=<username> as a query parameter'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    try:
-        user = User.objects.get(username=username)
-        if request.method == 'GET':
-            if user.user_type == "admin":
-                stories = Story.objects.all()
-            elif user.user_type == "user":
-                stories = Story.objects.filter(created_by=user.id)
-            if story_status is not None:
-                stories = stories.filter(status=story_status.upper())
-            serializer = StoryRetrieveBriefSerializer(stories, context={'request': request}, many=True)
-            return Response(serializer.data)
-        elif request.method == 'POST':
-            story = request.data
-            story['created_by'] = user.id
-            story['last_updated_by'] = user.id
-            story['status'] = "DRAFT"
-            serializer = StoryCreateSerializer(data=story)
-            if serializer.is_valid():
-                new_story = serializer.save()
-                return Response({'id': new_story.id}, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    except User.DoesNotExist as err:
-        return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e:
-        return Response({'error': e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # handling retrival of stories
+    if request.method == 'GET':
+        if user.user_type == "admin":
+            stories = Story.objects.all()
+        elif user.user_type == "user":
+            stories = Story.objects.filter(created_by=user.id)
+        if story_status is not None:
+            stories = stories.filter(status=story_status.upper())
+        serializer = StoryRetrieveBriefSerializer(stories, context={'request': request}, many=True)
+        return Response(serializer.data)
+
+    # handling creation of stories
+    elif request.method == 'POST':
+        story = request.data
+        story['created_by'] = user.id
+        story['last_updated_by'] = user.id
+        story['status'] = "DRAFT"
+        serializer = StoryCreateSerializer(data=story)
+        if serializer.is_valid():
+            new_story = serializer.save()
+            return Response({'id': new_story.id}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -51,42 +73,38 @@ def story_detail(request, pk):
     """
     Retrieve, update or delete a product instance.
     """
-    username = request.query_params.get('user', None)
-    if username == None:
-        return Response({'error':'specify user=<username> as a query parameter'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    try:
+
+    user = check_user_validity(request)
+    if (type(user)) != User:
+        return Response({'error': error_messages['user'][user]}, status=status.HTTP_400_BAD_REQUEST)
+
+    story = check_story_validity(pk)
+    if type(story) != Story:
+        return Response({'error': error_messages['story'][story]}, status=status.HTTP_404_NOT_FOUND)
+
+    if not(user.user_type == "admin" or user.id == story.created_by.id):
+        return Response({'error': 'Unauthorized access'},status=status.HTTP_401_UNAUTHORIZED)
+
+    if request.method == 'GET':
+        serializer = StoryRetrieveSerializer(story,context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        username = request.query_params.get('user', None)
         user = User.objects.get(username=username)
-        try:
-            story = Story.objects.get(pk=pk)
-        except Story.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        if not(user.user_type == "admin" or user.id == story.created_by.id):
-            return Response({'error': 'Unauthorized access'},status=status.HTTP_401_UNAUTHORIZED)
-
-        if request.method == 'GET':
-            serializer = StoryRetrieveSerializer(story,context={'request': request})
+        if story['status'] in ['APPROVED', 'REJECTED']:
+            story['status'] = 'DRAFT'
+        story['last_updated_by'] = user.id
+        serializer = StoryCreateSerializer(story, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        elif request.method == 'PUT':
-            username = request.query_params.get('user', None)
-            user = User.objects.get(username=username)
-            story['last_updated_by'] = user.id
-            serializer = StoryCreateSerializer(story, data=request.data,context={'request': request})
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        story.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-        elif request.method == 'DELETE':
-            story.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-    except User.DoesNotExist as err:
-        return Response({'error': 'User does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e:
-        return Response({'error': e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(['GET', 'POST'])
 def users_list(request):
@@ -109,10 +127,9 @@ def user_detail(request, pk):
     """
     Retrieve, update or delete a product instance.
     """
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    user = check_user_validity(request)
+    if (type(user)) != User:
+        return Response({'error': error_messages['user'][user]}, status=status.HTTP_404_BAD_REQUEST)
 
     if request.method == 'GET':
         serializer = UserSerializer(user,context={'request': request})
@@ -131,19 +148,14 @@ def user_detail(request, pk):
 
 @api_view(['PUT'])
 def story_submit(request, pk):
-    try:
-        story = Story.objects.get(pk=pk)
-    except Story.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    username = request.query_params.get('user', None)
-    if username == None:
-        return Response({'error': 'specify user=<username> as a query parameter'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = check_user_validity(request)
+    if (type(user)) != User:
+        return Response({'error': error_messages['user'][user]}, status=status.HTTP_400_BAD_REQUEST)
+
+    story = check_story_validity(pk)
+    if type(story) != Story:
+        return Response({'error': error_messages['story'][story]}, status=status.HTTP_404_NOT_FOUND)
 
     if user.id == story.created_by.id:
         if story.status == 'DRAFT':
@@ -157,66 +169,38 @@ def story_submit(request, pk):
     else:
         return Response({'error': 'Unauthorized access'},status=status.HTTP_401_UNAUTHORIZED)
 
-@api_view(['PUT'])
-def story_approve(request, pk):
-    try:
-        story = Story.objects.get(pk=pk)
-    except Story.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    username = request.query_params.get('user', None)
-    if username == None:
-        return Response({'error': 'specify user=<username> as a query parameter'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+def story_approve_reject(request, pk, action):
+
+    user = check_user_validity(request)
+    if (type(user)) != User:
+        return Response({'error': error_messages['user'][user]}, status=status.HTTP_400_BAD_REQUEST)
+
+    story = check_story_validity(pk)
+    if type(story) != Story:
+        return Response({'error': error_messages['story'][story]}, status=status.HTTP_404_NOT_FOUND)
 
     if user.user_type == 'admin':
         if story.status == 'FOR REVIEW':
-            story.status = 'APPROVED'
+            story.status = action.upper() + 'ED'
             story.save()
-            return Response({'success': 'Story %d approved' % story.id},
+            return Response({'success': 'Story %d %sed' % (story.id, action)},
                             status=status.HTTP_202_ACCEPTED)
         elif story.status == 'APPROVED':
-            return Response({'response': 'Story %d is already approved' % story.id},
+            return Response({'response': 'Story %d is already %sed' % (story.id, action)},
                             status=status.HTTP_204_NO_CONTENT)
         else:
-            return Response({'response': 'Story %d in %s state cannot be approved' % (story.id, story.status)},
+            return Response({'response': 'Story %d in %s state cannot be %sed' % (story.id, story.status, action)},
                             status=status.HTTP_204_NO_CONTENT)
     else:
-        return Response({'error': 'Unauthorized access! Only admins can approve stories.'},
+        return Response({'error': 'Unauthorized access! Only admins can approve/reject stories.'},
                         status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['PUT'])
+def story_approve(request, pk):
+    return story_approve_reject(request, pk, 'approv')
 
 @api_view(['PUT'])
 def story_reject(request, pk):
-    try:
-        story = Story.objects.get(pk=pk)
-    except Story.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    username = request.query_params.get('user', None)
-    if username == None:
-        return Response({'error': 'specify user=<username> as a query parameter'},
-                        status=status.HTTP_400_BAD_REQUEST)
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    if user.user_type == 'admin':
-        if story.status == 'FOR REVIEW':
-            story.status = 'REJECTED'
-            story.save()
-            return Response({'success': 'Story %d rejected' % story.id},
-                            status=status.HTTP_202_ACCEPTED)
-        elif story.status == 'REJECTED':
-            return Response({'response': 'Story %d is already rejected' % story.id},
-                            status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response({'response': 'Story %d in %s state cannot be rejected' % (story.id, story.status)},
-                            status=status.HTTP_204_NO_CONTENT)
-    else:
-        return Response({'error': 'Unauthorized access! Only admins can reject stories.'},
-                        status=status.HTTP_401_UNAUTHORIZED)
+    return story_approve_reject(request, pk, 'reject')
